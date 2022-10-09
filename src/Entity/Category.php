bd0a9ce21b7ce04c7732f0efefb8798646d6c9dc
chahinez <chahinezsahaloui207@gmail.com>
2022-10-09 23:14:37 +0100

    first commit

---
+++ src/Entity/Category.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\Column(length:100)]
+    private ?string $ref = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getRef(): ?string
+    {
+        return $this->ref;
+    }
+
+    public function setRef(string $ref): self
+    {
+        $this->ref = $ref;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
